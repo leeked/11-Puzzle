91b9fc5b835583593012e20d10728dfd9281c18b
Kevin Lee <78504440+leeked@users.noreply.github.com>
2021-11-04 15:00:05 -0400

    Fixed Syntax and began blueprinting.
    Notes in notebook for how to handle transition.

--- node.py
+++ node.py
@@ -1,5 +1,7 @@
 class Node:
-	def __init__(self, depth = 0, path_cost = 0, state = []):
+	def __init__(self, depth=0, path_cost=0, state=None):
+		if state is None:
+			state = []
 		self.depth = depth
 		self.path_cost = path_cost
 		self.state = state
--- puzzle.py
+++ puzzle.py
@@ -3,20 +3,22 @@ import sys
 from heapq import heapify, heappush, heappop	# Heap data structure for priority queue			
 
 # Third-Party Libraries
-import node										# Node data structure
+import Node										# Node data structure
 
 
 # Heuristic
 def manhattan(state):
 	pass
-	
+
+
 # Expand
 def expand():
 	pass
 
+
 # Search
 def search(ini_state, goal_state):
-	node = Node(0, manhattan(ini_state), ini_state)
+	node = Node.Node(0, manhattan(ini_state), ini_state)
 
 
 # Parsing Input
