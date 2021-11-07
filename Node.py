aeba94c21a00cf50a709989bee585f414158aab8
Kevin Lee <78504440+leeked@users.noreply.github.com>
2021-11-07 18:46:36 -0500

    Added __repr__ to class definition

--- Node.py
+++ Node.py
@@ -1,13 +1,18 @@
 class Node:
-	def __init__(self, depth=0, path_cost=0, total_cost=0, state=None, parent=None, action=None):
-		if state is None:
-			state = []
-		self.total_cost = total_cost
-		self.action = action
-		self.parent = parent
-		self.depth = depth
-		self.path_cost = path_cost
-		self.state = state
+    def __init__(self, depth=0, path_cost=0, total_cost=0, state=None, parent=None, action=None):
+        if state is None:
+            state = []
+        self.total_cost = total_cost
+        self.action = action
+        self.parent = parent
+        self.depth = depth
+        self.path_cost = path_cost
+        self.state = state
 
-	def __lt__(self, other):
-		return self.path_cost < other.path_cost
+    def __lt__(self, other):
+        return self.path_cost < other.path_cost
+
+    def __repr__(self):
+        return "depth = % s , path_cost = % s , total_cost = % s , state = % s , action = % s" % (
+            self.depth, self.path_cost,
+            self.total_cost, self.state, self.action)
