49d835af168784ba3c65e33caf7f71db67001f44
Kevin Lee <78504440+leeked@users.noreply.github.com>
2021-11-07 17:31:03 -0500

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -18,7 +18,31 @@ def expand():
 
 # Search
 def search(ini_state, goal_state):
+	# Initialize Root Node
 	node = Node.Node(0, manhattan(ini_state), ini_state)
+
+	# Initialize frontier
+	frontier = [node]
+	heapify(frontier)
+
+	# Initialize Visited Hash Map
+	visited = {}
+
+	# Start Search from Frontier
+	while len(frontier) > 0:
+		next_node = frontier.pop()
+
+		# If Goal Node is Found
+		if next_node.state == goal_state:
+			return next_node
+
+		# Expand Children
+		for child in expand(next_node):
+			s = child.state
+
+			if s not in visited or child.path_cost < visited[s].path_cost:
+				visited[s] = child
+				heappush(frontier, (child.path_cost, child))
 
 
 # Main
@@ -47,8 +71,8 @@ def main():
 			ini_state.extend([int(n) for n in line.split(' ')])
 		count_line += 1
 
-	print(ini_state)
-	print(goal_state)
+	# Start Search
+	search(ini_state, goal_state)
 
 
 if __name__ == "__main__":
